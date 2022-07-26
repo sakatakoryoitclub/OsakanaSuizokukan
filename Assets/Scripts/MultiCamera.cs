b1517134208461ade8948953ee2e2473ffd832d9
AtaruMatsudaira <matsu222212@gmail.com>
2022-07-26 14:46:20 +0900

    first commit

---
+++ Assets/Scripts/MultiCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MultiCameras : MonoBehaviour
+{
+	[SerializeField, Range( 1, 8 )]
+	private int m_useDisplayCount   = 2;
+
+	private void Awake()
+	{
+		int count   = Mathf.Min( Display.displays.Length, m_useDisplayCount );
+
+		for( int i = 0; i < count; ++i )
+		{
+			Display.displays[ i ].Activate();
+		}
+	}
+
+} // class GameController
---
+++ Assets/Scripts/PixelCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public class PixelCtrl{
+
+	[RuntimeInitializeOnLoadMethod]
+
+	static void OnRuntimeMethodLoad()
+{
+
+			Screen.SetResolution( 3250, 1080, false, 60);
+
+
+	}
+
+}
